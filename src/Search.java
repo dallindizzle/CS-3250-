a791afa6e74bd1aa35cb25a8d252b14b93189b8d
dallindizzle <dallindizzle@gmail.com>
2017-09-15 16:10:37 -0600

    Added rest of search methods and added command line parameter

--- src/Search.java
+++ src/Search.java
@@ -3,19 +3,25 @@ import java.util.*;
 
 public class Search {
     public static void main(String[] args) {
-        Scanner fin = null;
-        try {
-            fin = new Scanner(new File("wordSearch1.txt"));
-        } catch (FileNotFoundException x) {
-            System.out.println("File open failed.");
-            x.printStackTrace();
+
+        if (args.length < 1) {
+            System.out.println("No arguments");
+        }
+        else {
+            Scanner fin = null;
+            try {
+                fin = new Scanner(new File(args[0]));
+            } catch (FileNotFoundException x) {
+                System.out.println("File open failed.");
+                x.printStackTrace();
+                System.exit(0);
+            }
+
+            Puzzle p = new Puzzle();
+            p.Read(fin);
+            p.Find();
+
             System.exit(0);
         }
-
-        Puzzle p = new Puzzle();
-        p.Read(fin);
-        p.Find();
-
-        System.exit(0);
     }
 }
